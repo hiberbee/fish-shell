799bf2e9099e57dbc352544b6a07c5adac9c5a13
Vlad Volkov <vlad.volkov@bndigital.co>
2022-05-08 21:18:08 +0300

    Init script update

--- init.sh
+++ init.sh
@@ -18,21 +18,30 @@ i686) architecture="386" ;;
 x86_64) architecture="amd64" ;;
 arm) dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
 esac
-
-
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher_version=4.3.0
-fisher_download_url="https://raw.githubusercontent.com/jorgebucaran/fisher/$fisher_version/fisher.fish"
+fisher_download_url="https://raw.githubusercontent.com/jorgebucaran/fisher/main/fisher.fish"
 starship_download_url="https://starship.rs/install.sh"
 fzf_version=0.27.0
-fzf_dowload_url="https://github.com/junegunn/fzf-bin/releases/download/$fzf_version/fzf-$fzf_version-${platform}_$architecture.tgz"
-fish_addons_url="https://raw.githubusercontent.com/Hiberbee/fish-theme/master/.config"
-xxh_url="https://github.com/xxh/xxh-portable/raw/master/result/xxh-portable-musl-alpine-Linux-x86_64.tar.gz"
+fzf_download_url="https://github.com/junegunn/fzf-bin/releases/download/$fzf_version/fzf-$fzf_version-${platform}_$architecture.tgz"
+fish_theme_url="https://raw.githubusercontent.com/hiberbee/fish-shell/master/.config/fish/functions/fish_prompt.fish"
+fish_addons_url="https://raw.githubusercontent.com/hiberbee/fish-shell/master/.config"
 
 config_dir=${XDG_CONFIG_HOME:-"$HOME/.config"}
 fish_functions_dir="$config_dir/fish/functions"
 bin_dir=${BIN_DIR:-$HOME/bin}
 
 echo -e "${green}Detected OS: ${cyan}$platform & $architecture${transparent}"
+
+if [ $platform == "darwin"]; then
+  echo -e "${yellow}Installing Fish shell${transparent}"
+  brew install fish
+fi
+
+if [ $platform == "linux"]; then
+  echo -e "${yellow}Installing Fish shell${transparent}"
+  apt install fish-shell
+fi
 
 if [ ! -d "$bin_dir" ]; then
   echo -e "${yellow}Directory '$bin_dir' directory is not exist, creating...${transparent}"
@@ -46,18 +55,9 @@ if [ ! -d "$fish_functions_dir" ]; then
   mkdir -p "$fish_functions_dir"
 fi
 
-
-echo -e "${yellow}Installing Fish Shell addons pack:${transparent}"
-if [ ! "$(command -v xxh)" ]; then
-  echo -e "${cyan}xxh${yellow} binary was not found in PATH $bin_dir, installing...${transparent}"
-  wget $xxh_url -qO- | tar -xzv -C "$bin_dir"
-else
-  echo -e "${cyan}xxh${green} already installed.${transparent}"
-fi
-
 if [ ! -f "$fish_functions_dir/fisher.fish" ]; then
   echo -e "${yellow}Downloading fisher...${transparent}"
-  wget "$fisher_download_url" -qO "$fish_functions_dir/fisher.fish"
+  curl -sL $fisher_download_url | source && fisher install jorgebucaran/fisher
 else
   echo -e "${cyan}fisher${green} already installed.${transparent}"
 fi
@@ -67,7 +67,6 @@ wget "$fish_addons_url/fish/fishfile" -qO "$config_dir/fish/fishfile"
 echo -e "${yellow}Configuring Fish Shell color theme...${transparent}"
 wget "$fish_addons_url/fish/functions/fish_greeting.fish" -qO "$config_dir/fish/functions/fish_greeting.fish"
 wget "$fish_addons_url/fish/functions/fish_prompt.fish" -qO "$config_dir/fish/functions/fish_prompt.fish"
-
 
 if [ ! "$(command -v starship)" ]; then
   echo -e "${yellow}Installing Starship shell prompt...${transparent}"
@@ -80,7 +79,7 @@ wget "$fish_addons_url/starship.toml" -qO "$config_dir/starship.toml"
 
 if [ ! "$(command -v fzf)" ]; then
   echo -e "${yellow}Downloading ${cyan}fzf v${fzf_version}${yellow}...${transparent}"
-  wget "$fzf_dowload_url" -qO- | tar -xzv -C "$bin_dir"
+  wget "$fzf_download_url" -qO- | tar -xzv -C "$bin_dir"
 else
   echo -e "${green}fzf already installed.${transparent}"
 fi
